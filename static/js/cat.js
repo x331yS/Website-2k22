0062ff8f459502c0db9ad8f25902dd62324808ba
FLEJOSNE <florian.lejosne@ynov.com>
2022-05-15 16:17:41 +0200

    [ADD] cat nrv + sunglasses

--- static/js/cat.js
+++ static/js/cat.js
@@ -22,19 +22,26 @@ function happycat() {
     document.getElementsByClassName("typing-animation")[8].style.animationDuration = "1200ms";
 }
 function badcat() {
-    paw_left_down.style.animation = "blinkcat 200ms infinite";
-    paw_left_down.style.animationDelay = "100ms";
-    paw_left_up.style.animation = "blinkcat 200ms infinite";
-    paw_right_down.style.animation = "blinkcat 200ms infinite";
-    document.getElementsByClassName("typing-animation")[0].style.animationDuration = "1200ms";
-    document.getElementsByClassName("typing-animation")[1].style.animationDuration = "1200ms";
-    document.getElementsByClassName("typing-animation")[2].style.animationDuration = "1200ms";
-    document.getElementsByClassName("typing-animation")[3].style.animationDuration = "1200ms";
-    document.getElementsByClassName("typing-animation")[4].style.animationDuration = "1200ms";
-    document.getElementsByClassName("typing-animation")[5].style.animationDuration = "1200ms";
-    document.getElementsByClassName("typing-animation")[6].style.animationDuration = "1200ms";
-    document.getElementsByClassName("typing-animation")[7].style.animationDuration = "1200ms";
-    document.getElementsByClassName("typing-animation")[8].style.animationDuration = "1200ms";
-    document.getElementsByClassName("nrv")[0].style.animationDelay = "0s";
-    document.getElementsByClassName("nrv")[1].style.animationDelay = "0s";
+    if (document.getElementsByClassName("nrv")[0].style.animationDelay === "5s"){
+        document.getElementsByClassName("sun")[0].style.opacity = "1";
+        document.getElementsByClassName("eye")[0].style.opacity = "0";
+        document.getElementsByClassName("eye")[1].style.opacity = "0";
+        paw_left_down.style.animation = "blinkcat 100ms infinite";
+        paw_left_down.style.animationDelay = "50ms";
+        paw_left_up.style.animation = "blinkcat 100ms infinite";
+        paw_right_down.style.animation = "blinkcat 100ms infinite";
+        document.getElementsByClassName("typing-animation")[0].style.animationDuration = "400ms";
+        document.getElementsByClassName("typing-animation")[1].style.animationDuration = "400ms";
+        document.getElementsByClassName("typing-animation")[2].style.animationDuration = "400ms";
+        document.getElementsByClassName("typing-animation")[3].style.animationDuration = "400ms";
+        document.getElementsByClassName("typing-animation")[4].style.animationDuration = "400ms";
+        document.getElementsByClassName("typing-animation")[5].style.animationDuration = "400ms";
+        document.getElementsByClassName("typing-animation")[6].style.animationDuration = "400ms";
+        document.getElementsByClassName("typing-animation")[7].style.animationDuration = "400ms";
+        document.getElementsByClassName("typing-animation")[8].style.animationDuration = "400ms";
+    } else {
+    happycat()
+    document.getElementsByClassName("nrv")[0].style.animationDelay = "5s";
+    document.getElementsByClassName("nrv")[1].style.animationDelay = "5s";
+    }
 }
