641dd9b9bf0d070ec53ba1c8a9e705f52f074920
Florian Lejosne <florian.lejosne@ynov.com>
2022-06-20 15:31:04 +0200

    [ADD] project section and test update

---
+++ static/js/sticky.js
@@ -0,0 +1,19 @@
+// window.onscroll = function() {myFunction()};
+//
+// // Get the navbar
+// let navbar = document.getElementById("stickyNav");
+//
+// // Get the offset position of the navbar
+// let sticky = navbar.offsetTop;
+// console.log(sticky)
+//
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//     if (window.pageYOffset >= sticky) {
+//         navbar.classList.add("sticky")
+//         // navbar.classList.remove("navDisplay");
+//     } else {
+//         navbar.classList.remove("sticky");
+//         // navbar.classList.add("navDisplay");
+//     }
+// }
