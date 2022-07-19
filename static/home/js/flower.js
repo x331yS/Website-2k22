b41f9dd5e042f4196054464e9bf0220032184f49
FLEJOSNE <florian.lejosne@ynov.com>
2022-07-19 11:53:13 +0200

    [ADD] french word

--- static/home/js/flower.js
+++
@@ -1,8 +0,0 @@
-const element = document.getElementById("flower");
-
-onload = () => {
-    const c = setTimeout(() => {
-        element.classList.remove("not-loaded");
-        clearTimeout(c);
-    }, 1000);
-};
---
+++ static/home/js/language.js
@@ -0,0 +1,26 @@
+function replaceLanguage() {
+    // 1st
+
+    // Title
+
+    document.getElementById('language_title').style.display = "none";
+    document.getElementById('language_title_fr').style.display = "block";
+
+
+    //Navbar 1
+    document.getElementById('language_navbar_home').style.display = "none";
+    document.getElementById('language_navbar_home_fr').style.display = "block";
+
+    document.getElementById('language_navbar_resume').style.display = "none";
+    document.getElementById('language_navbar_resume_fr').style.display = "block";
+
+    document.getElementById('language_navbar_about').style.display = "none";
+    document.getElementById('language_navbar_about_fr').style.display = "block";
+
+    document.getElementById('language_navbar_projet').style.display = "none";
+    document.getElementById('language_navbar_projet_fr').style.display = "block";
+
+    document.getElementById('language_description').style.display = "none";
+    document.getElementById('language_description_fr').style.display = "block"
+
+}
