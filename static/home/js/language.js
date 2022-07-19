6a870f001eb3b6f11d6b728fbe45170e16f13102
FLEJOSNE <florian.lejosne@ynov.com>
2022-07-19 12:23:51 +0200

    [ADD] french word

--- static/home/js/language.js
+++ static/home/js/language.js
@@ -23,4 +23,26 @@ function replaceLanguage() {
     document.getElementById('language_description').style.display = "none";
     document.getElementById('language_description_fr').style.display = "block"
 
+    document.getElementById('language_letter').style.display = "none";
+    document.getElementById('language_letter_fr').style.display = "block"
+
+    //2nd
+
+    document.getElementById('language_about').style.display = "none";
+    document.getElementById('language_about_fr').style.display = "block"
+
+    document.getElementById('language_about_second').style.display = "none";
+    document.getElementById('language_about_second_fr').style.display = "block"
+
+    document.getElementById('language_about_third').style.display = "none";
+    document.getElementById('language_about_third_fr').style.display = "block"
+
+    document.getElementById('language_about_four').style.display = "none";
+    document.getElementById('language_about_four_fr').style.display = "block"
+
+
+
+
+
+
 }
