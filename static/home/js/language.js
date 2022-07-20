87257121e90a90f6c87d70151ae2e658b422f7e0
Florian Lejosne <florian.lejosne@ynov.com>
2022-07-20 14:11:24 +0200

    [ADD] Auto push from shell file

--- static/home/js/language.js
+++ static/home/js/language.js
@@ -1,11 +1,10 @@
 function replaceLanguage() {
-    if (window.location.href.split('/').pop() === "#en" || window.location.href.split('/').pop() === "") {
+    if (window.location.href.split('/').pop() === "#fr" ){
+        replaceEnglish()
+    } else {
         window.location.replace("#fr")
         replaceFrench()
     }
-    console.log(getfullURL())
-
-
 }
 
 function getfullURL() {
@@ -135,5 +134,126 @@ function replaceFrench() {
         }
     }
 }
+function replaceEnglish() {
+    if (getfullURL() === "en" || window.location.href.split('/').pop() === "") {
+            // 1st
+
+            // Title
+            document.getElementById('language_title').style.display = "block";
+            document.getElementById('language_title_fr').style.display = "none";
+
+
+            //Navbar 1
+            document.getElementById('language_navbar_home').style.display = "block";
+            document.getElementById('language_navbar_home_fr').style.display = "none";
+
+            document.getElementById('language_navbar_resume').style.display = "block";
+            document.getElementById('language_navbar_resume_fr').style.display = "none";
+
+            document.getElementById('language_navbar_about').style.display = "block";
+            document.getElementById('language_navbar_about_fr').style.display = "none";
+
+            document.getElementById('language_navbar_projet').style.display = "block";
+            document.getElementById('language_navbar_projet_fr').style.display = "none";
+
+            document.getElementById('language_description').style.display = "block";
+            document.getElementById('language_description_fr').style.display = "none";
+
+            document.getElementById('language_letter').style.display = "block";
+            document.getElementById('language_letter_fr').style.display = "none";
+
+            //2nd
+
+            document.getElementById('language_about').style.display = "block";
+            document.getElementById('language_about_fr').style.display = "none";
+
+            document.getElementById('language_about_second').style.display = "block";
+            document.getElementById('language_about_second_fr').style.display = "none";
+
+            document.getElementById('language_about_third').style.display = "block";
+            document.getElementById('language_about_third_fr').style.display = "none";
+
+            document.getElementById('language_about_four').style.display = "block";
+            document.getElementById('language_about_four_fr').style.display = "none";
+
+            document.getElementById('language_work').style.display = "block";
+            document.getElementById('language_work_fr').style.display = "none";
+
+
+            document.getElementById('language_work_second').style.display = "block";
+            document.getElementById('language_work_second_fr').style.display = "none";
+
+            document.getElementById('language_undercube').style.display = "block";
+            document.getElementById('language_undercube_fr').style.display = "none";
+
+            document.getElementById('language_latestproject').style.display = "block";
+            document.getElementById('language_latestproject_fr').style.display = "none";
+
+            document.getElementById('language_photoproject').style.display = "block";
+            document.getElementById('language_photoproject_fr').style.display = "none";
+
+            document.getElementById('language_codingproject').style.display = "block";
+            document.getElementById('language_codingproject_fr').style.display = "none";
+
+            document.getElementById('language_musicproject').style.display = "block";
+            document.getElementById('language_musicproject_fr').style.display = "none";
+
+            // 3rd
+
+            document.getElementById('language_allproject').style.display = "block";
+            document.getElementById('language_allproject_fr').style.display = "none";
+
+            //Navbar 2
+            document.getElementById('language_navbar_home_second').style.display = "block";
+            document.getElementById('language_navbar_home_fr_second').style.display = "none";
+
+            document.getElementById('language_navbar_resume_second').style.display = "block";
+            document.getElementById('language_navbar_resume_fr_second').style.display = "none";
+
+            document.getElementById('language_navbar_about_second').style.display = "block";
+            document.getElementById('language_navbar_about_fr_second').style.display = "none";
+
+            document.getElementById('language_navbar_projet_second').style.display = "block";
+            document.getElementById('language_navbar_projet_fr_second').style.display = "none";
+
+
+            document.getElementById('language_projet_musical').style.display = "block";
+            document.getElementById('language_projet_musical_fr').style.display = "none";
+
+            document.getElementById('language_projet_photo').style.display = "block";
+            document.getElementById('language_projet_photo_fr').style.display = "none";
+
+            document.getElementById('language_projet_code').style.display = "block";
+            document.getElementById('language_projet_code_fr').style.display = "none";
+
+            document.getElementById('language_getintouch').style.display = "block";
+            document.getElementById('language_getintouch_fr').style.display = "none";
+
+            document.getElementById('language_preloader').style.display = "block";
+            document.getElementById('language_preloader_fr').style.display = "none";
+
+
+            document.getElementById('language_last_project').setAttribute('style',"display:block!important");
+
+
+            document.getElementById('language_last_project_fr').setAttribute('style',"display:none!important");
+
+
+
+            document.getElementById('language_form_fr').style.display = "none";
+            document.getElementById('language_form').setAttribute('style',"display:flex!important");
+
+
+
+        }
+
+        if (window.location.href.split('/').pop() === "izanami.html#fr") {
+            console.log("good boy")
+
+        } else {
+            console.log("good")
+        }
+}
+
 
 replaceFrench()
