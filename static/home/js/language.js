ceb70d97f6481ace3d5ab7436d4621d9344619b6
Florian Lejosne <florian.lejosne@ynov.com>
2022-07-20 13:10:06 +0200

    [ADD] Auto push from shell file

--- static/home/js/language.js
+++ static/home/js/language.js
@@ -15,106 +15,106 @@ function replaceFrench() {
             // 1st
 
             // Title
-            document.getElementById('language_title').style.display = "none!important";
-            document.getElementById('language_title_fr').style.display = "block!important";
+            document.getElementById('language_title').style.display = "none";
+            document.getElementById('language_title_fr').style.display = "block";
 
 
             //Navbar 1
-            document.getElementById('language_navbar_home').style.display = "none!important";
-            document.getElementById('language_navbar_home_fr').style.display = "block!important";
+            document.getElementById('language_navbar_home').style.display = "none";
+            document.getElementById('language_navbar_home_fr').style.display = "block";
 
-            document.getElementById('language_navbar_resume').style.display = "none!important";
-            document.getElementById('language_navbar_resume_fr').style.display = "block!important";
+            document.getElementById('language_navbar_resume').style.display = "none";
+            document.getElementById('language_navbar_resume_fr').style.display = "block";
 
-            document.getElementById('language_navbar_about').style.display = "none!important";
-            document.getElementById('language_navbar_about_fr').style.display = "block!important";
+            document.getElementById('language_navbar_about').style.display = "none";
+            document.getElementById('language_navbar_about_fr').style.display = "block";
 
-            document.getElementById('language_navbar_projet').style.display = "none!important";
-            document.getElementById('language_navbar_projet_fr').style.display = "block!important";
+            document.getElementById('language_navbar_projet').style.display = "none";
+            document.getElementById('language_navbar_projet_fr').style.display = "block";
 
-            document.getElementById('language_description').style.display = "none!important";
-            document.getElementById('language_description_fr').style.display = "block!important";
+            document.getElementById('language_description').style.display = "none";
+            document.getElementById('language_description_fr').style.display = "block";
 
-            document.getElementById('language_letter').style.display = "none!important";
-            document.getElementById('language_letter_fr').style.display = "block!important";
+            document.getElementById('language_letter').style.display = "none";
+            document.getElementById('language_letter_fr').style.display = "block";
 
             //2nd
 
-            document.getElementById('language_about').style.display = "none!important";
-            document.getElementById('language_about_fr').style.display = "block!important";
+            document.getElementById('language_about').style.display = "none";
+            document.getElementById('language_about_fr').style.display = "block";
 
-            document.getElementById('language_about_second').style.display = "none!important";
-            document.getElementById('language_about_second_fr').style.display = "block!important";
+            document.getElementById('language_about_second').style.display = "none";
+            document.getElementById('language_about_second_fr').style.display = "block";
 
-            document.getElementById('language_about_third').style.display = "none!important";
-            document.getElementById('language_about_third_fr').style.display = "block!important";
+            document.getElementById('language_about_third').style.display = "none";
+            document.getElementById('language_about_third_fr').style.display = "block";
 
-            document.getElementById('language_about_four').style.display = "none!important";
-            document.getElementById('language_about_four_fr').style.display = "block!important";
+            document.getElementById('language_about_four').style.display = "none";
+            document.getElementById('language_about_four_fr').style.display = "block";
 
-            document.getElementById('language_work').style.display = "none!important";
-            document.getElementById('language_work_fr').style.display = "block!important";
+            document.getElementById('language_work').style.display = "none";
+            document.getElementById('language_work_fr').style.display = "block";
 
 
-            document.getElementById('language_work_second').style.display = "none!important";
-            document.getElementById('language_work_second_fr').style.display = "block!important";
+            document.getElementById('language_work_second').style.display = "none";
+            document.getElementById('language_work_second_fr').style.display = "block";
 
-            document.getElementById('language_undercube').style.display = "none!important";
-            document.getElementById('language_undercube_fr').style.display = "block!important";
+            document.getElementById('language_undercube').style.display = "none";
+            document.getElementById('language_undercube_fr').style.display = "block";
 
-            document.getElementById('language_latestproject').style.display = "none!important";
-            document.getElementById('language_latestproject_fr').style.display = "block!important";
+            document.getElementById('language_latestproject').style.display = "none";
+            document.getElementById('language_latestproject_fr').style.display = "block";
 
-            document.getElementById('language_photoproject').style.display = "none!important";
-            document.getElementById('language_photoproject_fr').style.display = "block!important";
+            document.getElementById('language_photoproject').style.display = "none";
+            document.getElementById('language_photoproject_fr').style.display = "block";
 
-            document.getElementById('language_codingproject').style.display = "none!important";
-            document.getElementById('language_codingproject_fr').style.display = "block!important";
+            document.getElementById('language_codingproject').style.display = "none";
+            document.getElementById('language_codingproject_fr').style.display = "block";
 
-            document.getElementById('language_musicproject').style.display = "none!important";
-            document.getElementById('language_musicproject_fr').style.display = "block!important";
+            document.getElementById('language_musicproject').style.display = "none";
+            document.getElementById('language_musicproject_fr').style.display = "block";
 
             // 3rd
 
-            document.getElementById('language_allproject').style.display = "none!important";
-            document.getElementById('language_allproject_fr').style.display = "block!important";
+            document.getElementById('language_allproject').style.display = "none";
+            document.getElementById('language_allproject_fr').style.display = "block";
 
             //Navbar 2
-            document.getElementById('language_navbar_home_second').style.display = "none!important";
-            document.getElementById('language_navbar_home_fr_second').style.display = "block!important";
+            document.getElementById('language_navbar_home_second').style.display = "none";
+            document.getElementById('language_navbar_home_fr_second').style.display = "block";
 
-            document.getElementById('language_navbar_resume_second').style.display = "none!important";
-            document.getElementById('language_navbar_resume_fr_second').style.display = "block!important";
+            document.getElementById('language_navbar_resume_second').style.display = "none";
+            document.getElementById('language_navbar_resume_fr_second').style.display = "block";
 
-            document.getElementById('language_navbar_about_second').style.display = "none!important";
-            document.getElementById('language_navbar_about_fr_second').style.display = "block!important";
+            document.getElementById('language_navbar_about_second').style.display = "none";
+            document.getElementById('language_navbar_about_fr_second').style.display = "block";
 
-            document.getElementById('language_navbar_projet_second').style.display = "none!important";
-            document.getElementById('language_navbar_projet_fr_second').style.display = "block!important";
+            document.getElementById('language_navbar_projet_second').style.display = "none";
+            document.getElementById('language_navbar_projet_fr_second').style.display = "block";
 
 
-            document.getElementById('language_projet_musical').style.display = "none!important";
-            document.getElementById('language_projet_musical_fr').style.display = "block!important";
+            document.getElementById('language_projet_musical').style.display = "none";
+            document.getElementById('language_projet_musical_fr').style.display = "block";
 
-            document.getElementById('language_projet_photo').style.display = "none!important";
-            document.getElementById('language_projet_photo_fr').style.display = "block!important";
+            document.getElementById('language_projet_photo').style.display = "none";
+            document.getElementById('language_projet_photo_fr').style.display = "block";
 
-            document.getElementById('language_projet_code').style.display = "none!important";
-            document.getElementById('language_projet_code_fr').style.display = "block!important";
+            document.getElementById('language_projet_code').style.display = "none";
+            document.getElementById('language_projet_code_fr').style.display = "block";
 
-            document.getElementById('language_sartre').style.display = "none!important";
-            document.getElementById('language_sartre_fr').style.display = "block!important";
+            document.getElementById('language_sartre').style.display = "none";
+            document.getElementById('language_sartre_fr').style.display = "block";
 
-            document.getElementById('language_redpanda').style.display = "none!important";
-            document.getElementById('language_redpanda_fr').style.display = "block!important";
+            document.getElementById('language_redpanda').style.display = "none";
+            document.getElementById('language_redpanda_fr').style.display = "block";
 
-            document.getElementById('language_lonewolf').style.display = "none!important";
-            document.getElementById('language_lonewolf_fr').style.display = "block!important";
+            document.getElementById('language_lonewolf').style.display = "none";
+            document.getElementById('language_lonewolf_fr').style.display = "block";
 
-            document.getElementById('language_getintouch').style.display = "none!important";
-            document.getElementById('language_getintouch_fr').style.display = "block!important";
+            document.getElementById('language_getintouch').style.display = "none";
+            document.getElementById('language_getintouch_fr').style.display = "block";
 
-            document.getElementById('language_form').style.display = "none!important";
+            document.getElementById('language_form').style.display = "none";
             document.getElementById('language_form_fr').style.display = "flex!important";
 
 
