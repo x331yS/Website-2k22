e19977280d1ec6da4d481b0b524648c297df9f73
Florian Lejosne <florian.lejosne@ynov.com>
2022-07-21 10:54:58 +0200

    [ADD] Auto push from shell file

--- static/home/js/index.js
+++ static/home/js/index.js
@@ -136,3 +136,19 @@ function linkedin() {
         document.getElementById("linkedin").style.stroke='currentcolor';
     }
 }
+
+// Swiper
+
+var mySwiper = new Swiper(".swiper-container", {
+    direction: "vertical",
+    loop: true,
+    pagination: ".swiper-pagination",
+    grabCursor: true,
+    speed: 1000,
+    paginationClickable: true,
+    parallax: true,
+    autoplay: true,
+    effect: "slide",
+    keyboardControl: true,
+    mousewheelControl: 0
+});
